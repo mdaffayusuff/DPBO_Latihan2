7e45a49e801c5fdab7352e7c1de37d4202983384
mdaffayusuff <95558620+mdaffayusuff@users.noreply.github.com>
2023-02-21 20:37:48 +0700

    Update SivitasAkademik.php

--- php/SivitasAkademik.php
+++ php/SivitasAkademik.php
@@ -1,6 +1,6 @@
 <?php
 
-/*Saya Muhammad Daffa Yusuf Fadhilah dengan NIM 2100543 mengerjakan evaluasi Latihan 
+/*Saya Muhammad Daffa Yusuf Fadhilah dengan NIM 2100543 mengerjakan evaluasi Latihan 2
 dalam mata kuliah Design Pemrograman Berorientasi Objek
 untuk keberkahanNya maka saya tidak melakukan kecurangan seperti
 yang telah dispesifikasikan. Aamiin. */
